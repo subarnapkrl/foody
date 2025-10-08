40e5907cc5dcbd62705905b1fab5b129b482b8ac
Soobarna <subarnapokharel252@gmail.com>
2025-10-08 16:11:34 +0545

    fix: changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-4 items-center">
+      <h1 className="text-5xl text-center mt-4">FOODIE</h1>
+      <Button>
+        <Link href={"/client"}>Visit Signup</Link>
+      </Button>
+    </div>
+  );
 }
